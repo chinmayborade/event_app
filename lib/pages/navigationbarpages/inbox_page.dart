a82276780d90c1a7ec8c3793e57e63741ffa1e4b
Chinmay Borade <chinmayycodes@gmail.com>
2024-10-01 22:16:27 +0530

    chat page  and menu page done

--- lib/pages/navigationbarpages/inbox_page.dart
+++ lib/pages/navigationbarpages/inbox_page.dart
@@ -1,14 +1,273 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class InboxPage extends StatelessWidget {
+  const InboxPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Inbox Page',
-        style: GoogleFonts.poppins(fontSize: 24),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: false,
+        elevation: 0,
+        backgroundColor: HexColor("B8A1DC"),
+        foregroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+        title: Text(
+          "Chats",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+            color: HexColor("B8A1DC"),
+            child: Row(
+              children: [
+                FillOutlineButton(press: () {}, text: "Recent Message"),
+                const SizedBox(width: 16.0),
+                FillOutlineButton(
+                  press: () {},
+                  text: "Active",
+                  isFilled: false,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: chatsData.length,
+              itemBuilder: (context, index) => ChatCard(
+                chat: chatsData[index],
+                press: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: HexColor("B8A1DC"),
+        child: const Icon(
+          Icons.person_add_alt_1,
+          color: Colors.white,
+        ),
       ),
     );
   }
 }
+
+class ChatCard extends StatelessWidget {
+  const ChatCard({
+    super.key,
+    required this.chat,
+    required this.press,
+  });
+
+  final Chat chat;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: press,
+      child: Padding(
+        padding:
+            const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0 * 0.75),
+        child: Row(
+          children: [
+            CircleAvatarWithActiveIndicator(
+              image: chat.image,
+              isActive: chat.isActive,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      chat.name,
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.w500),
+                    ),
+                    const SizedBox(height: 8),
+                    Opacity(
+                      opacity: 0.64,
+                      child: Text(
+                        chat.lastMessage,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Opacity(
+              opacity: 0.64,
+              child: Text(chat.time),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FillOutlineButton extends StatelessWidget {
+  const FillOutlineButton({
+    super.key,
+    this.isFilled = true,
+    required this.press,
+    required this.text,
+  });
+
+  final bool isFilled;
+  final VoidCallback press;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+        side: const BorderSide(color: Colors.white),
+      ),
+      elevation: isFilled ? 2 : 0,
+      color: isFilled ? Colors.white : Colors.transparent,
+      onPressed: press,
+      child: Text(
+        text,
+        style: TextStyle(
+          color: isFilled ? const Color(0xFF1D1D35) : Colors.white,
+          fontSize: 12,
+        ),
+      ),
+    );
+  }
+}
+
+class CircleAvatarWithActiveIndicator extends StatelessWidget {
+  const CircleAvatarWithActiveIndicator({
+    super.key,
+    this.image,
+    this.radius = 24,
+    this.isActive,
+  });
+
+  final String? image;
+  final double? radius;
+  final bool? isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        CircleAvatar(
+          radius: radius,
+          backgroundImage: NetworkImage(image!),
+        ),
+        if (isActive!)
+          Positioned(
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: 16,
+              width: 16,
+              decoration: BoxDecoration(
+                color: const Color(0xFF00BF6D),
+                shape: BoxShape.circle,
+                border: Border.all(
+                    color: Theme.of(context).scaffoldBackgroundColor, width: 3),
+              ),
+            ),
+          )
+      ],
+    );
+  }
+}
+
+class Chat {
+  final String name, lastMessage, image, time;
+  final bool isActive;
+
+  Chat({
+    this.name = '',
+    this.lastMessage = '',
+    this.image = '',
+    this.time = '',
+    this.isActive = false,
+  });
+}
+
+List chatsData = [
+  Chat(
+    name: "Jenny Wilson",
+    lastMessage: "Hope you are doing well...",
+    image: "https://i.postimg.cc/g25VYN7X/user-1.png",
+    time: "3m ago",
+    isActive: false,
+  ),
+  Chat(
+    name: "Esther Howard",
+    lastMessage: "Hello Abdullah! I am...",
+    image: "https://i.postimg.cc/cCsYDjvj/user-2.png",
+    time: "8m ago",
+    isActive: true,
+  ),
+  Chat(
+    name: "Ralph Edwards",
+    lastMessage: "Do you have update...",
+    image: "https://i.postimg.cc/sXC5W1s3/user-3.png",
+    time: "5d ago",
+    isActive: false,
+  ),
+  Chat(
+    name: "Jacob Jones",
+    lastMessage: "You’re welcome :)",
+    image: "https://i.postimg.cc/4dvVQZxV/user-4.png",
+    time: "5d ago",
+    isActive: true,
+  ),
+  Chat(
+    name: "Albert Flores",
+    lastMessage: "Thanks",
+    image: "https://i.postimg.cc/FzDSwZcK/user-5.png",
+    time: "6d ago",
+    isActive: false,
+  ),
+  Chat(
+    name: "Jenny Wilson",
+    lastMessage: "Hope you are doing well...",
+    image: "https://i.postimg.cc/g25VYN7X/user-1.png",
+    time: "3m ago",
+    isActive: false,
+  ),
+  Chat(
+    name: "Esther Howard",
+    lastMessage: "Hello Abdullah! I am...",
+    image: "https://i.postimg.cc/cCsYDjvj/user-2.png",
+    time: "8m ago",
+    isActive: true,
+  ),
+  Chat(
+    name: "Ralph Edwards",
+    lastMessage: "Do you have update...",
+    image: "https://i.postimg.cc/sXC5W1s3/user-3.png",
+    time: "5d ago",
+    isActive: false,
+  ),
+];
--- lib/pages/navigationbarpages/menu_page.dart
+++ lib/pages/navigationbarpages/menu_page.dart
@@ -4,11 +4,55 @@ import 'package:google_fonts/google_fonts.dart';
 class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Menu Page',
-        style: GoogleFonts.poppins(fontSize: 24),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: Text('Menu', style: GoogleFonts.poppins()),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Circular Profile Image
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage:
+                  NetworkImage('https://i.postimg.cc/cCsYDjvj/user-2.png'),
+              // Placeholder for when image fails to load
+              child: Icon(Icons.person, size: 50.0),
+            ),
+            SizedBox(height: 20),
+            // List of Options
+            Expanded(
+              child: ListView(
+                children: [
+                  _buildListTile(Icons.account_circle, 'My Account'),
+                  SizedBox(height: 15),
+                  _buildListTile(Icons.notifications, 'Notifications'),
+                  SizedBox(height: 15),
+                  _buildListTile(Icons.settings, 'Settings'),
+                  SizedBox(height: 15),
+                  _buildListTile(Icons.help, 'Help Center'),
+                  SizedBox(height: 15),
+                  _buildListTile(Icons.logout, 'Logout'),
+                  SizedBox(height: 15),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildListTile(IconData icon, String title) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(title, style: GoogleFonts.poppins()),
+      onTap: () {
+        // Handle tap
+      },
     );
   }
 }
